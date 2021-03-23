17ec00c3a5e567441c369e2426c2947f316bd351
fuminori-ikeuchi <fuminori.7127.lv19@icloud.com>
2021-03-23 20:34:37 +0900

    stocksテーブル,Homeコントローラー作成

---
+++ backend/app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Home;   //このファイルはどの階層にあるか
+
+use App\Http\Controllers\Controller;
+// use App\Stock; 最初に書かれていた(useは何を使うか)
+use Illuminate\Http\Request;
+use app\Models\Stock;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [
+            // 'stock'      => Stock::getBypk(2)
+            "stock"         => "welcome!"
+        ];
+
+        return view('stock.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stock $stock)
+    {
+        //
+    }
+}
+
